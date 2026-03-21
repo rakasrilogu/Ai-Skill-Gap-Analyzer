58bbbbb64763495743a79cac74096f21c2b75318
rakasrilogu <rakasrilogu@gmail.com>
2026-03-21 18:49:22 +0530

    Delete SkillBridge Pro AI/pdf_utils.py

--- SkillBridge Pro AI/pdf_utils.py
+++
@@ -1,8 +0,0 @@
-import fitz
-
-def extract_text(uploaded_file):
-    text = ""
-    pdf = fitz.open(stream=uploaded_file.read(), filetype="pdf")
-    for page in pdf:
-        text += page.get_text()
-    return text
